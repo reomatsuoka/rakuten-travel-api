cb1bcdd4964a456d8ac05ae56da6b23a81cb6354
reomatsuoka <reo199637458@gmail.com>
2021-03-04 00:14:36 +0900

    詳細画面作成中

--- app/views.py
+++ app/views.py
@@ -80,7 +80,6 @@ class IndexView(View, LoginRequiredMixin):
                     travel_data.append(query)
                 # 降順に並び替え
 
-
             return render(request, 'app/travel.html', {
                 'travel_data': travel_data,
                 'keyword':keyword,
@@ -90,3 +89,53 @@ class IndexView(View, LoginRequiredMixin):
         'form': 'form'
         })
         
+class DetailView(View):
+    def get(self, request, *args, **kwargs):
+        hotelno = self.kwargs['hotelno']
+        params = {
+            'hotelno': hotelno,
+        }
+        
+        result = get_api_data(params)
+        hotel0 = result['hotels'][0]['hotel'][0]
+        summary = hotel0['hotelBasicInfo']
+        hotelname = summary['hotelName']
+        hotelno = summary['hotelNo']
+        image = summary['hotelImageUrl']
+        review = summary['reviewAverage']
+        reviewCount = summary['reviewCount']
+        plan = summary['planListUrl']
+        minprice = summary['hotelMinCharge']
+        hotel_detail = summary['hotelSpecial']
+        address = summary['address1']
+        address2 = summary['address2']
+
+        hotel1 = result['hotels'][0]['hotel'][1]
+        rating_info = hotel1['hotelRatingInfo']
+        rating_service = rating_info['serviceAverage']
+        rating_room = rating_info['roomAverage']
+        rating_equipment = rating_info['equipmentAverage']
+        rating_meal = rating_info['mealAverage']
+        rating_location = rating_info['locationAverage']
+        travel_data = {
+            'hotelname': hotelname,
+            'hotelno': hotelno,
+            'image': image,
+            'review': review,
+            'reviewCount': reviewCount,
+            'plan': plan,
+            'minprice': minprice,
+            'hotel_detail': hotel_detail,
+            'address':address,
+            'address2': address2,
+            'rating_service': rating_service,
+            'rating_room': rating_room,
+            'rating_equipment': rating_equipment,
+            'rating_meal': rating_meal,
+            'rating_location': rating_location,
+            'average': float(review) * 20,
+        }
+
+        return render(request, 'app/detail.html', {
+            'travel_data': travel_data,
+        })
