8bde2703795d11dfa6894436a6bda7fafbf8436d
reomatsuoka <reo199637458@gmail.com>
2021-03-23 17:49:15 +0900

    paginator

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,8 @@ from app import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    # indexViewとSearchViewを分けるのはなぜ？
+    path('search/', views.SearchView.as_view(), name='search'),
     path('detail/<str:hotelno>', views.DetailView.as_view(), name='detail'),
     path('favorite/', views.FavoriteView.as_view(), name='favorite'),
 ]
--- app/views.py
+++ app/views.py
@@ -8,7 +8,6 @@ import json
 from .forms import SearchForm
 
 
-
 REQUEST_VACANCY_URL = 'https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?format=json&applicationId=1077422512724458210'
 REQUEST_URL = 'https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?format=json&applicationId=1077422512724458210'
 REQUEST_DETAIL_URL = 'https://app.rakuten.co.jp/services/api/Travel/HotelDetailSearch/20170426?format=json&applicationId=1077422512724458210'
@@ -17,18 +16,18 @@ json_file = open('app/areacode.json', 'r')
 # JSONとして読み込む
 json_obj  = json.load(json_file)
 
-# print(json_obj['middleClasses'])
-
 
 def get_api_data(params):
     res = requests.get(REQUEST_URL, params)
     result = res.json()
     return result
 
+
 def get_api_detail_data(params):
     res = requests.get(REQUEST_DETAIL_URL, params)
     result = res.json()
     return result
+
 
 def paginate_queryset(request, queryset, count):
     paginator = Paginator(queryset, count)
@@ -43,98 +42,84 @@ def paginate_queryset(request, queryset, count):
 
 class IndexView(View, LoginRequiredMixin):
     login_url = '/accounts/login/'
+    
     def get(self, request, *args, **kwargs):
         form = SearchForm(request.POST or None)
         return render(request, 'app/index.html', {
             'form': form,
         })
 
-    def post(self, request, *args, **kwargs):
-        form = SearchForm(request.POST or None)
+class SearchView(View, LoginRequiredMixin):
+    def get(self, request, *args, **kwargs):
+        checkin_date = request.GET['checkin_date']
+        checkout_date = request.GET['checkout_date']
+        middle_class_name = request.GET['middle_class_name']
+        small_class_name = request.GET['small_class_name']
 
-        if form.is_valid():
-            checkin_date = form.cleaned_data['checkin_date']
-            checkout_date = form.cleaned_data['checkout_date']
-            middle_class_name = form.cleaned_data['middle_class_name']
-            small_class_name = form.cleaned_data['small_class_name']
+        params = {
+            'checkinDate': checkin_date,
+            'checkoutDate': checkout_date,
+            'largeClassCode': "japan",
+            'middleClassCode': middle_class_name,
+            'smallClassCode': small_class_name,
+        }
+        # print(params)
+        result = get_api_data(params)
+        if 'hotels' not in result:
+            print('空室がありません')
+            return redirect('index')
 
-            # for i in json_obj['middleClasses']:
-            #     if middle_class_name == i['middleClass'][0]['middleClassName']:
-            #         middle_class_name = i['middleClass'][0]['middleClassCode']
-            #         small_classes = i['middleClass'][1]['smallClasses']
-            #         for j in small_classes:
-            #             if small_class_name in j['smallClass'][0]['smallClassName']:
-            #                 small_class_name = j['smallClass'][0]['smallClassCode']
-            #                 print(small_class_name)
-                    # print(i['middleClass'][1]['smallClasses'][0])
+        travel_data = []
+        for i in result['hotels']:
 
-            
-                
-            params = {
-                'checkinDate': checkin_date.strftime('%Y-%m-%d'),
-                'checkoutDate': checkout_date.strftime('%Y-%m-%d'),
-                'largeClassCode': "japan",
-                'middleClassCode': middle_class_name,
-                'smallClassCode': small_class_name,
+            hotel0 = i['hotel'][0]
+            summary = hotel0['hotelBasicInfo']
+            hotelname = summary['hotelName']
+            hotelno = summary['hotelNo']
+            image = summary['hotelImageUrl']
+            review = summary['reviewAverage']
+            plan = summary['planListUrl']
+            minprice = summary['hotelMinCharge']
+            hotel_detail = summary['hotelSpecial']
+            address = summary['address1']
+            address2 = summary['address2']
+            dpPlanListUrl = summary['dpPlanListUrl']
+            reviewUrl = summary['reviewUrl']
+
+            # hotel1 = i['hotel'][1]
+            # hotelReserveInfo = hotel1['hotelReserveInfo']
+            # reserveRecordCount = hotelReserveInfo['reserveRecordCount']
+
+            query = {
+                'hotelname': hotelname,
+                'hotelno': hotelno,
+                'image': image,
+                'review': review,
+                'plan': plan,
+                'minprice': minprice,
+                'hotel_detail': hotel_detail,
+                'address':address,
+                'address2': address2,
+                'dpPlanListUrl': dpPlanListUrl,
+                'reviewUrl': reviewUrl,
             }
-            # print(params)
-            result = get_api_data(params)
-            if not False:
-                travel_data = []
-                for i in result['hotels']:
+            # データがない宿泊施設を除外する
+            if not isinstance(review, float):
+                continue
+            # reviewが4.0以上のみ表示する
+            if review >= 4.0:
+                travel_data.append(query)
+        # 降順に並び替え
+        travel_data = sorted(travel_data, key=lambda x: x['review'], reverse=True)
+        total_hit_count = len(travel_data)
+        page_obj = paginate_queryset(request, travel_data, 10)
 
-                    hotel0 = i['hotel'][0]
-                    summary = hotel0['hotelBasicInfo']
-                    hotelname = summary['hotelName']
-                    hotelno = summary['hotelNo']
-                    image = summary['hotelImageUrl']
-                    review = summary['reviewAverage']
-                    plan = summary['planListUrl']
-                    minprice = summary['hotelMinCharge']
-                    hotel_detail = summary['hotelSpecial']
-                    address = summary['address1']
-                    address2 = summary['address2']
-                    dpPlanListUrl = summary['dpPlanListUrl']
-                    reviewUrl = summary['reviewUrl']
+        return render(request, 'app/travel.html', {
+            'travel_data': page_obj.object_list,
+            'total_hit_count':total_hit_count,
+            'page_obj': page_obj,
+        })
 
-                    # hotel1 = i['hotel'][1]
-                    # hotelReserveInfo = hotel1['hotelReserveInfo']
-                    # reserveRecordCount = hotelReserveInfo['reserveRecordCount']
-
-                    query = {
-                        'hotelname': hotelname,
-                        'hotelno': hotelno,
-                        'image': image,
-                        'review': review,
-                        'plan': plan,
-                        'minprice': minprice,
-                        'hotel_detail': hotel_detail,
-                        'address':address,
-                        'address2': address2,
-                        'dpPlanListUrl': dpPlanListUrl,
-                        'reviewUrl': reviewUrl,
-                    }
-                    # データがない宿泊施設を除外する
-                    if not isinstance(review, float):
-                        continue
-                    # reviewが4.0以上のみ表示する
-                    if review >= 4.0:
-                        travel_data.append(query)
-                # 降順に並び替え
-                travel_data = sorted(travel_data, key=lambda x: x['review'], reverse=True)
-                total_hit_count = len(travel_data)
-                page_obj = paginate_queryset(request, travel_data, 10)
-
-                return render(request, 'app/travel.html', {
-                    'travel_data': page_obj.object_list,
-                    'total_hit_count':total_hit_count,
-                    'page_obj': page_obj,
-                })
-            
-            return render(request, 'app/index.html', {
-            'form': 'form'
-            })
-        
 class DetailView(View):
     def get(self, request, *args, **kwargs):
         hotelno = self.kwargs['hotelno']
